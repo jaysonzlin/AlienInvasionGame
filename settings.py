8a6b5587f2c16a5deafbd61bc852167430305bbf
Jayson Lin <jlin9811@gmail.com>
2022-06-10 11:24:19 -0400

    Set up game class and updated settings class

--- settings.py
+++ settings.py
@@ -1,4 +1,9 @@
 # Settings
 class Settings:
 	'''A class to store all settings for Alien Invasion'''
-	wrong = 'test'
+	def __init__(self):
+		'''Initialize the game's settings.'''
+		#Screen settings
+		self.screen_width = 1200
+		self.screen_height = 800
+		self.bg_color = (230,230,230)
